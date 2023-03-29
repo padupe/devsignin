a49cb6645cfdc15ffa5123305f6921394248d9e2
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-03-29 09:23:30 -0300

    feat(src/main.ts): config "ValidationPipe".
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  // Configuração para que as validações sejam executadas através do 'class-validator'
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
 }
 bootstrap()
