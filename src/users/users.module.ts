dbd6fa7c1342e8fe42deadecbe992badf57f9875
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-03-29 09:21:19 -0300

    feat(src/users/users.module.ts): update @Module with "imports". Import "MongooseModule" and "AuthModule".
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { AuthModule } from 'src/auth/auth.module'
+import { usersSchema } from './schemas/users.schema'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: usersSchema,
+      },
+    ]),
+    AuthModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
